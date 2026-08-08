3512ec63a1daf82390fc32be5f0cd0d184829900
bradlycheng <bradlycheng@gmail.com>
2026-08-08 04:04:45 -0400

    book model lock check_out

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,38 +11,38 @@
 #     true
 #   end
 # end
-# class Book < ApplicationRecord
-#   # Returns true if this call performed the checkout, false if the
-#   # book was already checked out. The lock closes the gap between
-#   # reading checked_out and writing it.
-#   def check_out!
-#     with_lock do
-#       return false if checked_out?
-#       update!(checked_out: true)
-#       true
-#     end
-#   end
-
-#   def check_in!
-#     with_lock do
-#       return false unless checked_out?
-#       update!(checked_out: false)
-#       true
-#     end
-#   end
-# end
 class Book < ApplicationRecord
+  # Returns true if this call performed the checkout, false if the
+  # book was already checked out. The lock closes the gap between
+  # reading checked_out and writing it.
   def check_out!
-    updated = self.class.where(id: id, checked_out: false)
-                        .update_all(checked_out: true, updated_at: Time.current)
-    reload if updated == 1
-    updated == 1
+    with_lock do
+      return false if checked_out?
+      update!(checked_out: true)
+      true
+    end
   end
 
   def check_in!
-    updated = self.class.where(id: id, checked_out: true)
-                        .update_all(checked_out: false, updated_at: Time.current)
-    reload if updated == 1
-    updated == 1
+    with_lock do
+      return false unless checked_out?
+      update!(checked_out: false)
+      true
+    end
   end
 end
+# class Book < ApplicationRecord
+#   def check_out!
+#     updated = self.class.where(id: id, checked_out: false)
+#                         .update_all(checked_out: true, updated_at: Time.current)
+#     reload if updated == 1
+#     updated == 1
+#   end
+
+#   def check_in!
+#     updated = self.class.where(id: id, checked_out: true)
+#                         .update_all(checked_out: false, updated_at: Time.current)
+#     reload if updated == 1
+#     updated == 1
+#   end
+# end
