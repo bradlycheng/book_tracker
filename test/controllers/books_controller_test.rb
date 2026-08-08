897b98f51ce2a697d5b53fe9a3390e1cbd3ee16f
bradlycheng <bradlycheng@gmail.com>
2026-08-08 02:09:26 -0400

    Tests for basic checkout logic

---
+++ test/controllers/books_controller_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class BooksControllerTest < ActionDispatch::IntegrationTest
+  test "index lists books" do
+    Book.create!(title: "Refactoring", author: "Fowler")
+    get books_path
+    assert_response :success
+    assert_select "td", text: "Refactoring"
+  end
+
+  test "checking out an available book marks it checked out" do
+    book = Book.create!(title: "Test", author: "Author")
+    patch check_out_book_path(book)
+    assert book.reload.checked_out?
+  end
+
+  test "checking out an already checked out book does not succeed" do
+    book = Book.create!(title: "Test", author: "Author", checked_out: true)
+    patch check_out_book_path(book)
+    assert_redirected_to books_path
+    assert_equal "Already checked out.", flash[:alert]
+  end
+
+  test "returning a checked out book makes it available" do
+    book = Book.create!(title: "Test", author: "Author", checked_out: true)
+    patch check_in_book_path(book)
+    assert_not book.reload.checked_out?
+  end
+end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "new book defaults to available" do
+    book = Book.create!(title: "Test", author: "Author")
+    assert_not book.checked_out?
+  end
+
+  test "book can be checked out" do
+    book = Book.create!(title: "Test", author: "Author")
+    book.update!(checked_out: true)
+    assert book.reload.checked_out?
+  end
+
+  test "book can be returned" do
+    book = Book.create!(title: "Test", author: "Author", checked_out: true)
+    book.update!(checked_out: false)
+    assert_not book.reload.checked_out?
+  end
 end
