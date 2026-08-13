0e26b68789b5cb7c7d70e0342f24ed0301ce565c
bradlycheng <bradlycheng@gmail.com>
2026-08-13 04:49:02 -0400

    added first argument for query_type.rb

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,37 @@
 # frozen_string_literal: true
 
+# module Types
+#   class QueryType < Types::BaseObject
+#     field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
+#       argument :id, ID, required: true, description: "ID of the object."
+#     end
+
+#     def node(id:)
+#       context.schema.object_from_id(id, context)
+#     end
+
+#     field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
+#       argument :ids, [ID], required: true, description: "IDs of the objects."
+#     end
+
+#     def nodes(ids:)
+#       ids.map { |id| context.schema.object_from_id(id, context) }
+#     end
+
+#     field :books, [Types::BookType], null: false, description: "All books, ordered by title."
+#     def books
+#       Book.order(:title)
+#     end
+
+#     field :book, Types::BookType, null: true, description: "A single book by id." do
+#       argument :id, ID, required: true
+#     end
+#     def book(id:)
+#       Book.find_by(id: id)
+#     end
+#   end
+# end
+
 module Types
   class QueryType < Types::BaseObject
     field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
@@ -18,14 +50,19 @@ module Types
       ids.map { |id| context.schema.object_from_id(id, context) }
     end
 
-    field :books, [Types::BookType], null: false, description: "All books, ordered by title."
-    def books
-      Book.order(:title)
+    field :books, [Types::BookType], null: false, description: "All books, ordered by title." do
+      argument :first, Integer, required: false, description: "Max number of books to return."
+    end
+
+    def books(first: nil)
+      scope = Book.order(:title)
+      first ? scope.limit(first) : scope
     end
 
     field :book, Types::BookType, null: true, description: "A single book by id." do
       argument :id, ID, required: true
     end
+
     def book(id:)
       Book.find_by(id: id)
     end
